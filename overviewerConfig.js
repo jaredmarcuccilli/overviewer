657fc97e52da452ca6c9d7c8fa4e5943f482c8b8
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2023-06-13 00:03:24 -0400

    Add files via upload

---
+++ overviewerConfig.js
@@ -0,0 +1,130 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "VanillaCraft V3",
+        "VanillaCraft V4",
+        "SuperlativeCraft"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1669257645",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "VanillaCraft V3",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "VanillaCraft V3",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "VanillaCraft V3",
+            "last_rendertime": 1368797728,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                194,
+                95,
+                932
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                194,
+                95,
+                932
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "VanillaCraft V4",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "VanillaCraft V4",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "VanillaCraft V4",
+            "last_rendertime": 1417053503,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                88,
+                64,
+                42
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                88,
+                64,
+                42
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "SuperlativeCraft",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "SuperlativeCraft",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "SuperlativeCraft",
+            "last_rendertime": 1397699955,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -57,
+                113,
+                8
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -57,
+                113,
+                8
+            ],
+            "north_direction": 0
+        }
+    ]
+};
